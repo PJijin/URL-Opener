2d654aae7655e28294edeb326005105c894ac746
Pjijin <pjijin1@gmail.com>
2023-10-14 23:19:14 +0530

    🐛 Fix: updated to app dir

--- app/AppLayout.tsx
+++ app/AppLayout.tsx
@@ -1,13 +1,16 @@
+'use client';
+
 import { Theme } from '@radix-ui/themes';
-import type { AppProps } from 'next/app';
 import { Toaster } from 'sonner';
 import '../styles/global.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+const AppLayout = ({ children }) => {
 	return (
 		<Theme appearance="dark">
-			<Component {...pageProps} />
+			{children}
 			<Toaster />
 		</Theme>
 	);
-}
+};
+
+export default AppLayout;
